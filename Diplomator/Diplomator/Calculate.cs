e041f47fe62bee175378a97f10b0666f699ed934
Paiva <danpaiva@live.com>
2022-08-14 20:14:17 -0300

    First commit

---
+++ Diplomator/Diplomator/Calculate.cs
@@ -0,0 +1,22 @@
+﻿using static System.Console;
+
+namespace Diplomator
+{
+    public class Calculate
+    {
+        public static string CalculateDays()
+        {
+            DateTime dateUniversity = new(2022, 03, 29);
+            DateTime dateToday = DateTime.Now.Date;
+
+            var days = dateToday - dateUniversity;
+
+            return days.Days.ToString();
+        }
+
+        public void PrintDays(string days)
+        {
+            WriteLine($"\nInfelizmente a entrega do diploma está atrasada há: {days} DIAS.");
+        }
+    }
+}
---
+++ Diplomator/Diplomator/Program.cs
@@ -0,0 +1,21 @@
+﻿using Diplomator;
+using static System.Console;
+
+WriteLine("Diplomator");
+
+WriteLine("\nO diploma foi entregue?\nDigite: 1[SIM] 2[NAO]");
+int option = int.Parse(ReadLine()!);
+
+switch (option)
+{
+    case 1:
+        WriteLine("\nParabéns, Daniel você está graduado!");
+        break;
+    case 2:
+        Calculate calculate = new Calculate();
+        calculate.PrintDays(Calculate.CalculateDays());
+        break;
+    default:
+        Write("\nDigite a opção correta!");
+        break;
+}
