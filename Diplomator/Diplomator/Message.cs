9de5b01fe149066f66e6e64a5dffc73c969198a2
Paiva <danpaiva@live.com>
2022-08-14 22:26:30 -0300

    feat: refatorando classe Message

--- Diplomator/Diplomator/Message.cs
+++ Diplomator/Diplomator/Message.cs
@@ -4,34 +4,24 @@ namespace Diplomator
 {
     public class Message
     {
-        public static void Crocodile()
-        {
+        public static void Crocodile() =>
             WriteLine(@"
                   ─────▄████▀█▄
                   ───▄█████████████████▄
                   ─▄█████.▼.▼.▼.▼.▼.▼▼▼▼
                   ▄███████▄.▲.▲▲▲▲▲▲▲▲
                   ████████████████████▀▀");
-        }
 
-        public static void Sucess()
-        {
+        public static void Sucess() =>
             WriteLine("\n\tDaniel, Parabénssss! Você está oficialmente diplomadoooo!");
-        }
 
-        public static void Insucess(string days)
-        {
+        public static void Insucess(string days) =>
             WriteLine($"\n\tDaniel, Infelizmente a entrega do diploma está atrasada há: {days} DIAS.\n\tAté a próxima consulta!");
-        }
 
-        public static void QuestionOne()
-        {
+        public static void QuestionOne() =>
             WriteLine("\n\tDaniel, o seu diploma já foi entregue?\n\tDigite: 1[SIM] 2[NAO]");
-        }
 
-        public static void ErrorOption()
-        {
+        public static void ErrorOption() =>
             Write("\n\tDIGITE A OPÇÃO CORRETA!");
-        }
     }
 }
